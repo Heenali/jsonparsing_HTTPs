88a5e783935504f964d8cbdb8f54307c549a3605
Heenali Lakahni <Heenali@users.noreply.github.com>
2018-02-23 12:07:07 +0530

    Update AppConstant.java
    sss

--- app/src/main/java/com/example/jsonparsing_example/AppConstant.java
+++ app/src/main/java/com/example/jsonparsing_example/AppConstant.java
@@ -9,4 +9,32 @@ public class AppConstant
 	  public static int istablet=0;
 	 
 	  public static int positon=0;
-	}
+
+
+	/*json sqlite   : https://jumpshare.com/v/xDrvshKeL3CAZBZoxGfJ?b=TSTkE4WiQraG7kCOU3ps
+
+	json listing_posting :https://jumpshare.com/v/lk6FpXt9mBR6lRXZFF26
+
+	facebook key :802082836548724
+
+	Twitter key : <string name="twitter_consumer_key"
+		>kL6lv7GPNeBbV2KksspK1xZdi</string>
+    <string name="twitter_consumer_secret"  >6pDN3pt2HAZMS0tf3osNfysxO4nCxojvC01ecnKaJpt9RMHmVU</string>
+
+	map key :AIzaSyB3Cb1Jp7yQv6c_e5xpZAHx23yjGrrOHYs
+
+	json parsing :http://www.technotalkative.com/android-json-parsing/
+
+	auto place :AIzaSyD_Hvp-mAjMMMS_OgPFEIxqX-AsffaYK0E   (https://jumpshare.com/v/cqQJgemB3zBIt7nQWPrP)
+
+
+		Image uplode :https://jumpshare.com/v/Xc90mMpaPtVm51Tg23Um
+
+		recyler_lodemore :https://jumpshare.com/v/UYefBx7X01j1SUL5PM93
+
+		Shipper App :https://jumpshare.com/v/cqQJgemB3zBIt7nQWPrP*/
+
+
+
+
+}
